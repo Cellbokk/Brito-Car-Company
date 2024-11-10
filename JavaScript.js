afef041d48b6db99f7c4cc061c80547d7f988178
Cellbokk <daviquirinorohca34@gmail.com>
2024-11-10 12:51:11 -0300

    Primeiro commit

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+// script.js
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
